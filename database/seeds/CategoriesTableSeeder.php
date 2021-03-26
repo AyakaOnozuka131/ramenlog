b8fb3f88e2038d2184288e096d3b436bdc0fd68d
AyakaOnozuka131 <c_yu_again@icloud.com>
2021-03-26 21:11:29 +0900

    シーダーの作成

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            ['name' => '醤油ラーメン'],
+            ['name' => '塩ラーメン'],
+            ['name' => '味噌ラーメン'],
+            ['name' => '豚骨ラーメン']
+        ]);
+    }
+}
